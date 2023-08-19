55642e03b35105567842a0de989639409f7b2468
doni-zete <donizete112@gmail.com>
2023-08-18 22:41:00 -0300

    feat: pagina de sorvete adicionando função de selecionar

--- src/components/ProductList/index.js
+++ src/components/ProductList/index.js
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
-import ProductSorvete from "../ProductSorvete";
-import productsMock from "../../mock/products";
+import React, { useState } from 'react';
+import ProductSorvete from '../ProductSorvete';
+import productsMock from '../../mock/products';
 
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 const ProductList = () => {
-  const [categoriaTab, setCategoriaTab] = useState("Copo de Sorvete");
+  const [categoriaTab, setCategoriaTab] = useState('Copo de Sorvete');
   const [products] = useState(productsMock);
 
   return (
@@ -13,43 +13,43 @@ const ProductList = () => {
       <div className="flex items-center justify-center space-x-6">
         <p
           className={
-            categoriaTab === "Copo de Sorvete"
-              ? "active-menu-tab bg-primary"
-              : "menu-tab"
+            categoriaTab === 'Copo de Sorvete'
+              ? 'active-menu-tab bg-primary'
+              : 'menu-tab'
           }
-          onClick={() => setCategoriaTab("Copo de Sorvete")}
+          onClick={() => setCategoriaTab('Copo de Sorvete')}
         >
           Copo de Sorvete
         </p>
         <button
           className={
-            categoriaTab === "Picole"
-              ? "active-menu-tab bg-primary"
-              : "menu-tab"
+            categoriaTab === 'Picole'
+              ? 'active-menu-tab bg-primary'
+              : 'menu-tab'
           }
         >
-          <Link to="/picole" onClick={() => setCategoriaTab("Picole")}>
+          <Link to="/picole" onClick={() => setCategoriaTab('Picole')}>
             Picole
           </Link>
         </button>
         <p
           className={
-            categoriaTab === "Modelos novos"
-              ? "active-menu-tab bg-primary"
-              : "menu-tab"
+            categoriaTab === 'Modelos novos'
+              ? 'active-menu-tab bg-primary'
+              : 'menu-tab'
           }
-          onClick={() => setCategoriaTab("Modelos novos")}
+          onClick={() => setCategoriaTab('Modelos novos')}
         >
           <Link
             to="/poteSorvete"
-            onClick={() => setCategoriaTab("Pote de Sorvete")}
+            onClick={() => setCategoriaTab('Pote de Sorvete')}
           >
             Pote de Sorvete
           </Link>
         </p>
       </div>
 
-      <div className="grid  grid-cols-4">
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3">
         {products.map((product) => (
           <ProductSorvete key={product._id} product={product} />
         ))}
